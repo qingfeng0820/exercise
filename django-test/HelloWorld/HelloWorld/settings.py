f23d112f4fff89ec49cd900541d44bf7db1e45b2
sherman_sh <qingfeng0820@sina.com>
2018-03-10 22:45:04 +0800

    model test

--- django-test/HelloWorld/HelloWorld/settings.py
+++ django-test/HelloWorld/HelloWorld/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for HelloWorld project.
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'TestModel',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +77,17 @@ WSGI_APPLICATION = 'HelloWorld.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 或者使用 mysql.connector.django
+        'NAME': 'django_test',
+        'USER': 'dev',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- django-test/HelloWorld/HelloWorld/urls.py
+++ django-test/HelloWorld/HelloWorld/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from . import view
+from . import view,testdb
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello$', view.hello),
-    url(r'^greet$', view.greet)
+    url(r'^greet$', view.greet),
+    url(r'^testdb$', testdb.testdb),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
